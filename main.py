4c9ef4caec9595b05ff9f94f935e03f09cb8764d
n4sr <54733427+n4sr@users.noreply.github.com>
2019-12-09 08:00:01 -0600

    Simplified opacity/brightness application in construct_binds definition

--- main.py
+++ main.py
@@ -3,29 +3,12 @@ import csv
 import os
 from PIL import Image
 from PIL import ImageChops
+from PIL import ImageEnhance
 
 
 keys = ('q','e','r','f','c','v','b','mouse4','mouse5')
 opacity = 0.5
 offset = (1,1)
-
-
-def set_pixel_level(pixel, m):
-    r, g, b, a = pixel
-
-    if a == 0:
-        return (0, 0, 0, 0)
-    else:
-        return (round(r*m), round(g*m), round(b*m), a)
-
-
-def set_image_brightness(img, multiplier):
-    img_w, img_h = img.size
-
-    for x in range(img_w):
-        for y in range(img_h):
-            pos = (x,y)
-            img.putpixel(pos, set_pixel_level(img.getpixel(pos), multiplier))
 
 
 def construct_binds(keys, opacity=1, spacing=18):
@@ -35,11 +18,10 @@ def construct_binds(keys, opacity=1, spacing=18):
 
     for n, key in keys_enum:
         pos = n * spacing, 0
-        key_img = Image.open(f'keys/{key.lower()}.png')
+        key_img = Image.open(f'keys/{key.lower()}.png').convert('1')
         background.paste(key_img, pos)
 
-    set_image_brightness(background, opacity)
-    return background
+    return ImageEnhance.Brightness(background).enhance(opacity)
 
 
 def overlay_binds(gui, binds, position):
